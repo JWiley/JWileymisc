1cf88eca5853cde28fd9743818af3a7712d50f9d
Joshua F. Wiley, Ph.D <jwiley.psych@gmail.com>
2022-05-10 20:11:46 +1000

    remove all files from gh-pages branch

--- tests/testthat/test-star.R
+++
@@ -1,9 +0,0 @@
-test_that("star works", {
-  expect_equal(
-    star(c(.06, .05, .004)),
-    noquote(c("", "*", "**")))
-
-  expect_equal(
-    star(c(.06, .05, .004), includeMarginal = TRUE),
-    noquote(c("^", "*", "**")))
-})
--- tests/testthat/test-visual-acuity.R
+++
@@ -1,7 +0,0 @@
-test_that("VAConverter works", {
-  sampdat <- c("HM 12", "20/20 + 3", "20/50", "CF", "HM",
-               "20/70 - 2", "LP", NA, "Prosthetic")
-  tmp <- VAConverter(OS = sampdat, OD = rev(sampdat), datatype = "snellen")
-
-  expect_s4_class(tmp, "VAObject")
-})
